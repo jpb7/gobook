1ff352fbcf542b255a2073be1750ee5d1d30bfc9
Jacob Bentley <jacobpbentley@gmail.com>
2024-10-12 11:15:17 -0700

    Complete Ex 1.4

--- ch1/dup2/main.go
+++ ch1/dup2/main.go
@@ -16,9 +16,10 @@ import (
 
 func main() {
 	counts := make(map[string]int)
+	filenames := make(map[string][]string)
 	files := os.Args[1:]
 	if len(files) == 0 {
-		countLines(os.Stdin, counts)
+		countLines(os.Stdin, counts, filenames)
 	} else {
 		for _, arg := range files {
 			f, err := os.Open(arg)
@@ -26,23 +27,36 @@ func main() {
 				fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
 				continue
 			}
-			countLines(f, counts)
+			countLines(f, counts, filenames)
 			f.Close()
 		}
 	}
 	for line, n := range counts {
 		if n > 1 {
-			fmt.Printf("%d\t%s\n", n, line)
+			fmt.Printf("%d\t%s\t%v\n", n, line, filenames[line])
 		}
 	}
 }
 
-func countLines(f *os.File, counts map[string]int) {
+func countLines(f *os.File, counts map[string]int, filenames map[string][]string) {
 	input := bufio.NewScanner(f)
 	for input.Scan() {
-		counts[input.Text()]++
+		line := input.Text()
+		counts[line]++
+		if !contains(filenames[line], f.Name()) {
+			filenames[line] = append(filenames[line], f.Name())
+		}
 	}
 	// NOTE: ignoring potential errors from input.Err()
+}
+
+func contains(slice []string, val string) bool {
+	for _, item := range slice {
+		if item == val {
+			return true
+		}
+	}
+	return false
 }
 
 //!-
