9cc86b2506821c275dcc37dee8b4b301f98cdb68
Jacob Bentley <jpb7@pdx.edu>
2025-01-01 12:49:51 -0800

    Complete Exercise 1.6

---
+++ ch1/ex1.6/main.go
@@ -0,0 +1,112 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// Run with "web" command-line argument for web server.
+// See page 13.
+//!+main
+
+// Lissajous generates GIF animations of random Lissajous figures.
+package main
+
+import (
+	"flag"
+	"image"
+	"image/color"
+	"image/gif"
+	"io"
+	"log"
+	"math"
+	"math/rand"
+	"net/http"
+	"os"
+	"time"
+)
+
+//!-main
+// Packages not needed by version in book.
+
+//!+main
+
+//	Colors
+var red = color.RGBA{0xFF, 0x33, 0x33, 0xFF}
+var green = color.RGBA{0x33, 0xFF, 0x33, 0xFF}
+var blue = color.RGBA{0x33, 0x33, 0xFF, 0xFF}
+var yellow = color.RGBA{0xFF, 0xFF, 0x33, 0xFF}
+var palette = []color.Color{color.Black, green, red, blue, yellow}
+
+//	Flag
+var change = flag.Bool("change", false, "change colors across frames")
+
+const (
+	blackIndex  = 0
+	greenIndex  = 1
+	redIndex    = 2
+	blueIndex   = 3
+	yellowIndex = 4
+)
+
+func main() {
+	//!-main
+	flag.Parse()
+
+	// The sequence of images is deterministic unless we seed
+	// the pseudo-random number generator using the current time.
+	// Thanks to Randall McPherson for pointing out the omission.
+	rand.Seed(time.Now().UTC().UnixNano())
+
+	if len(os.Args) > 1 && os.Args[1] == "web" {
+		//!+http
+		handler := func(w http.ResponseWriter, r *http.Request) {
+			lissajous(w, *change)
+		}
+		http.HandleFunc("/", handler)
+		//!-http
+		log.Fatal(http.ListenAndServe("localhost:8000", nil))
+		return
+	}
+	//!+main
+	lissajous(os.Stdout, *change)
+}
+
+func lissajous(out io.Writer, changeColors bool) {
+	const (
+		cycles  = 5     // number of complete x oscillator revolutions
+		res     = 0.001 // angular resolution
+		size    = 100   // image canvas covers [-size..+size]
+		nframes = 64    // number of animation frames
+		delay   = 8     // delay between frames in 10ms units
+	)
+	freq := rand.Float64() * 3.0 // relative frequency of y oscillator
+	anim := gif.GIF{LoopCount: nframes}
+	phase := 0.0 // phase difference
+	colorIndex := byte(0)
+
+	for i := 0; i < nframes; i++ {
+		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
+		img := image.NewPaletted(rect, palette)
+
+		//	Change colors across frames
+		if changeColors {
+			colorIndex = byte(1 + i%4)
+		}
+
+		for t := 0.0; t < cycles*2*math.Pi; t += res {
+			x := math.Sin(t)
+			y := math.Sin(t*freq + phase)
+
+			//	Display multiple colors
+			if !changeColors {
+				colorIndex = byte(1 + int(t*100)%4)
+			}
+
+			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5),
+				colorIndex)
+		}
+		phase += 0.1
+		anim.Delay = append(anim.Delay, delay)
+		anim.Image = append(anim.Image, img)
+	}
+	gif.EncodeAll(out, &anim) // NOTE: ignoring encoding errors
+}
+
+//!-main
